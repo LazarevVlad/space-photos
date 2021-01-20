e788420ccd7dda7097769f706318863871b832e9
Лазарев Владислав <v.lazarev@machineheads.ru>
2021-01-20 09:53:03 +0700

    добавил вывод описания

--- src/components/Main.js
+++ src/components/Main.js
@@ -8,6 +8,7 @@ function Main() {
   const [img, setImg] = useState({
     src: '',
     alt: 'Ошибка',
+    text: ''
   })
 
   useEffect(() => {
@@ -15,7 +16,8 @@ function Main() {
     .then((res) => {
       setImg({
         src: res.url,
-        alt: res.title
+        alt: res.title,
+        text: res.explanation
       })
     })
     .catch((err) => console.log(`Ошибка ${err}`))
@@ -24,13 +26,14 @@ function Main() {
   return(
     <div className="main">
     <p className="main__title">Astronomy Picture of the Day</p>
-      <div className="main__img-container">
-        <img
-          className="main__img"
-          alt={img.alt}
-          src={img.src}
-        />
-      </div>
+    <div className="main__img-container">
+      <img
+        className="main__img"
+        alt={img.alt}
+        src={img.src}
+      />
+    </div>
+    <p className="main__text">{img.text}</p>
     </div>
   )
 }
