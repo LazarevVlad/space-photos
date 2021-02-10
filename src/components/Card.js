188200af86a53b7f20078e3cc931cfca59e1b59b
Лазарев Владислав <v.lazarev@machineheads.ru>
2021-02-10 12:51:55 +0700

    добавил регулярку для проверки приходящих данных

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,22 +1,37 @@
 import React from 'react';
+import { regExpImg } from '../utils/constants';
 
 function Card(props) {
   const { card, onCardClick } = props;
-
   function handleCardClick() {
     onCardClick(card.url, card.title)
   }
 
+  let cardTemplate;
+
+  if (regExpImg.test(card.url)) {
+    cardTemplate = <div className="card__img-container">
+    <img 
+      className="card__img"
+      src={card.url}
+      alt={card.title}
+      onClick={handleCardClick}
+    />
+  </div>
+  } else {
+    cardTemplate = <div className="card__iframe-container">
+      <iframe
+        src={card.url}
+        className="card__iframe"
+        allow="fullscreen"
+      />
+      <a href={card.url}>Link</a>
+    </div>
+  }
+
   return  (
     <div className="card">
-      <div className="card__img-container">
-        <img 
-          className="card__img"
-          src={card.url}
-          alt={card.title}
-          onClick={handleCardClick}
-        />
-      </div>
+      {cardTemplate}
       <div className="card__description">
         <p className="card__text">{card.explanation}</p>
         <p className="card__date">{card.date}</p>
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Pictures from "./Pictures";
 import api from '../utils/Api';
+import { regExpImg } from '../utils/constants';
+
 
 function Main(props) {
   const { onCardClick } = props;
@@ -23,16 +25,31 @@ function Main(props) {
     .catch((err) => console.log(`Ошибка ${err}`))
   }, [])
 
+  let cardTemplate;
+
+  if (regExpImg.test(img.src)) {
+    cardTemplate = <div className="main__img-container">
+    <img
+      className="main__img"
+      alt={img.alt}
+      src={img.src}
+    />
+  </div>
+  } else {
+    cardTemplate = <div className="main__iframe-container">
+      <iframe
+        src={img.src}
+        className="main__iframe"
+        allow="fullscreen"
+      />
+      <a href={img.src}>Link</a>
+    </div>
+  }
+
   return(
     <div className="main">
       <p className="main__title">Astronomy Picture of the Day</p>
-      <div className="main__img-container">
-        <img
-          className="main__img"
-          alt={img.alt}
-          src={img.src}
-        />
-      </div>
+      {cardTemplate}
       <p className="main__text">{img.text}</p>
       <Pictures
         onCardClick={onCardClick}
